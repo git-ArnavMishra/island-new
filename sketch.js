2c197adea2faa7f2ce47e768af3f61931603e7f2
Arnav <arnav123@gmail.com>
2021-01-23 15:43:18 -0800

    test.commit

--- sketch.js
+++ sketch.js
@@ -11,7 +11,7 @@ function preload(){
 }
 function setup() {
   createCanvas(displayWidth-200,displayHeight-240);
-  
+  //;
   man = new Man();
   playButton = createSprite((displayWidth-200)/2,displayHeight-440);
   playButton.addImage(playButtonImage);
